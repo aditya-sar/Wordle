95ab5cbb33e4485792c8b5422050b9c65630c0b3
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2023-08-02 21:20:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+const dictionary = [
+    'about', 'above', 'abuse', 'actor', 'acute', 'admit', 'adopt', 'adult', 'after', 'again', 'agent', 'agree', 'ahead', 'alarm', 'album', 'alert', 'alike', 'alive', 'allow', 'alone', 'along', 'alter', 'among', 'anger', 'angle', 'angry', 'apart', 'apple', 'apply', 'arena', 'argue', 'arise', 'array', 'aside', 'asset', 'audio', 'audit', 'avoid', 'award', 'aware', 'awful', 'badly', 'baker', 'bases', 'basic', 'basis', 'beach', 'began', 'begin', 'begun', 'being', 'below', 'bench', 'billy', 'birth', 'black', 'blame', 'blind', 'block', 'blood', 'board', 'boost', 'booth', 'bound', 'brain', 'brand', 'brave', 'bread', 'break', 'breed', 'brief', 'bring', 'broad', 'broke', 'brown', 'brush', 'build', 'built', 'buyer', 'cable', 'calif', 'carry', 'catch', 'cause', 'chain', 'chair', 'chart', 'chase', 'cheap', 'check', 'chest', 'chief', 'child', 'china', 'chord', 'chose', 'civil', 'claim', 'class', 'clean', 'clear', 'click', 'clock', 'close', 'cloud', 'coach', 'coast', 'could', 'count', 'court', 'cover', 'craft', 'crash', 'crazy', 'cream', 'crime', 'cross', 'crowd', 'crown', 'curve', 'cycle', 'daily', 'dance', 'dated', 'dealt', 'death', 'debut', 'delay', 'depth', 'diary', 'dirty', 'doing', 'doubt', 'dozen', 'draft', 'drama', 'drawn', 'dream', 'dress', 'drill', 'drink', 'drive', 'drove', 'dying', 'eager', 'early', 'earth', 'eight', 'elite', 'empty', 'enemy', 'enjoy', 'enter', 'entry', 'equal', 'error', 'event', 'every', 'exact', 'exist', 'extra', 'faint', 'faith', 'false', 'fault', 'fibre', 'field', 'fifth', 'fifty', 'fight', 'final', 'first', 'fixed', 'flash', 'fleet', 'floor', 'fluid', 'flute', 'focus', 'force', 'forth', 'forty', 'forum', 'found', 'frame', 'frank', 'fraud', 'fresh', 'front', 'fruit', 'fully', 'funny', 'ghost', 'giant', 'given', 'glass', 'globe', 'going', 'grace', 'grade', 'grand', 'grant', 'grape', 'grass', 'great', 'green', 'gross', 'group', 'grown', 'guard', 'guest', 'guide', 'happy', 'harry', 'harsh', 'heart', 'heavy', 'hence', 'henry', 'horse', 'hotel', 'house', 'human', 'ideal', 'image', 'index', 'inner', 'input', 'irony', 'issue', 'japan', 'joint', 'jones', 'judge', 'juice', 'knife', 'known', 'label', 'large', 'laser', 'later', 'laugh', 'laura', 'layer', 'learn', 'lease', 'least', 'leave', 'legal', 'level', 'lewis', 'light', 'limit', 'local', 'logic', 'loose', 'lower', 'lucky', 'lunch', 'lying', 'magic', 'major', 'maker', 'march', 'match', 'mayor', 'meant', 'media', 'metal', 'might', 'minor', 'minus', 'mixed', 'model', 'money', 'month', 'moral', 'motor', 'mount', 'mouse', 'mouth', 'movie', 'music', 'naked', 'nasty', 'naval', 'needs', 'never', 'newly', 'night', 'noise', 'north', 'noted', 'novel', 'nurse', 'occur', 'ocean', 'offer', 'often', 'order', 'other', 'ought', 'outer', 'owner', 'paint', 'panel', 'paper', 'party', 'peace', 'peter', 'phase', 'phone', 'photo', 'piece', 'pilot', 'pitch', 'pizza', 'place', 'plain', 'plane', 'plant', 'plate', 'point', 'pound', 'power', 'press', 'price', 'pride', 'prime', 'print', 'prior', 'prize', 'proof', 'proud', 'prove', 'queen', 'quick', 'quiet', 'quite', 'radio', 'raise', 'range', 'rapid', 'ratio', 'reach', 'ready', 'refer', 'reply', 'right', 'rival', 'river', 'roman', 'rough', 'round', 'route', 'royal', 'rugby', 'rural', 'salad', 'scale', 'scene', 'scope', 'score', 'sense', 'serve', 'seven', 'shall', 'shape', 'share', 'sharp', 'sheep', 'sheer', 'sheet', 'shelf', 'shell', 'shift', 'shirt', 'shock', 'shoes', 'shoot', 'short', 'shown', 'sight', 'silly', 'simon', 'since', 'sixth', 'sixty', 'sized', 'skill', 'sleep', 'slide', 'small', 'smart', 'smile', 'smith', 'smoke', 'snack', 'snake', 'solid', 'solve', 'sorry', 'sound', 'south', 'space', 'spare', 'speak', 'speed', 'spend', 'spent', 'spice', 'spite', 'split', 'spoke', 'spoon', 'sport', 'squad', 'staff', 'stage', 'stake', 'stand', 'start', 'state', 'steam', 'steel', 'steep', 'stick', 'still', 'stock', 'stone', 'stood', 'store', 'storm', 'story', 'strip', 'stuck', 'study', 'stuff', 'style', 'sugar', 'suite', 'super', 'sweet', 'table', 'taken', 'taste', 'taxes', 'teach', 'teeth', 'terry', 'texas', 'thank', 'theft', 'their', 'theme', 'there', 'these', 'thick', 'thing', 'think', 'third', 'those', 'three', 'threw', 'throw', 'tiger', 'tight', 'times', 'tired', 'title', 'toast', 'today', 'topic', 'total', 'touch', 'tough', 'tower', 'track', 'trade', 'train', 'treat', 'trend', 'trial', 'tried', 'tries', 'truck', 'truly', 'trust', 'truth', 'twice', 'uncle', 'under', 'undue', 'union', 'unity', 'until', 'upper', 'upset', 'urban', 'usage', 'usual', 'vague', 'valid', 'value', 'video', 'virus', 'visit', 'vital', 'voice', 'waste', 'watch', 'water', 'whale', 'wheel', 'where', 'which', 'while', 'white', 'whole', 'whose', 'woman', 'world', 'worry', 'worse', 'worst', 'worth', 'would', 'wound', 'write', 'wrong', 'wrote', 'yield', 'young', 'youth' 
+];
+
+
+const state = {
+    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
+    grid: Array(6).fill().map(() => Array(5).fill()), 
+    currentRow: 0, 
+    currentCol: 0,
+};
+
+
+function updateGrid() {
+    for(let i=0; i<state.grid.length; i++) {
+        for(let j=0; j<state.grid[i].length; j++) {
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
+
+function drawBox(container, row, col, letter = '') {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = `box${row}${col}`;
+    box.textContent = letter;
+    container.appendChild(box);
+    return box;
+}
+
+
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    for(let i=0; i<6; i++) {
+        for(let j=0; j<5; j++) {
+            drawBox(grid, i, j);
+        }
+    }
+    container.appendChild(grid);
+}
+
+
+function isLetter(key) {
+    return key.length === 1 && key.toLowerCase() !== key.toUpperCase();
+}
+
+
+function addLetter(letter) {
+    if(state.currentCol === 5) {
+        return;
+    }
+    state.grid[state.currentRow][state.currentCol] = letter;
+    state.currentCol++;
+}
+
+
+function removeLastLetter() {
+    if(state.currentCol === 0) {
+        return;
+    }
+    state.grid[state.currentRow][state.currentCol - 1] = '';
+    state.currentCol--;
+}
+
+
+function getCurrentWord(word) {
+    return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
+}
+
+
+function isValidWord(word) {
+    return dictionary.includes(word);
+}
+
+
+function revealWord(guess) {
+    const row = state.currentRow;
+    const animationDuration = 500;
+
+    for(let i=0; i<5; i++) {
+        const box = document.getElementById(`box${row}${i}`);
+        const letter = box.textContent;
+
+        setTimeout(() => {
+            if(letter === state.secret[i]) {
+                box.classList.add('right');
+            }
+            else if(state.secret.includes(letter)) {
+                box.classList.add('wrong');
+            }
+            else {
+                box.classList.add('empty');
+            }
+        }, ((i + 1) * animationDuration) / 2);
+
+        box.classList.add('animated');
+        box.style.animationDelay = `${(i * animationDuration) / 2}ms`;
+
+    }
+    
+    const isWinner = state.secret === guess;
+    const gameOver = state.currentRow === 5;
+
+    setTimeout(() => {
+        if(isWinner) {
+            alert("CONGRATS!");        
+        }
+        else if(gameOver) {
+            alert(`Game over. The correct word was ${state.secret.toUpperCase()}.`);
+        }        
+    }, 3 * animationDuration);
+}
+
+
+function recordInputs() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if(key === 'Enter') {
+            if(state.currentCol === 5) {
+                const word = getCurrentWord();
+                if(isValidWord(word)) {
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                }
+                else {
+                    alert("Not in word list!");
+                }
+            }
+        }
+        if(key === 'Backspace') {
+            removeLastLetter();
+        }
+        if(isLetter(key)) {
+            addLetter(key);
+        }
+        updateGrid();
+    };
+}
+
+
+function startGame() {
+    const game = document.getElementById('game');
+    drawGrid(game);
+    recordInputs();
+    console.log(`Word is ${state.secret.toUpperCase()}`);
+}
+
+startGame();
